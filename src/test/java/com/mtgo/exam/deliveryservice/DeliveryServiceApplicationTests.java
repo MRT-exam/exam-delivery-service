b582c616112e068729d41e6d91bdb90350eda5bb
Theagns <markus.agnsgaard@hotmail.com>
2023-12-05 12:44:31 +0100

    first

---
+++ src/test/java/com/mtgo/exam/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtgo.exam.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
