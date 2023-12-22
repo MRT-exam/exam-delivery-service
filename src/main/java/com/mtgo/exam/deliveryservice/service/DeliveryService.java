72bb304eda20c230c268cc53c5a3b3436af93abb
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-12-22 16:32:41 +0100

    small change

--- src/main/java/com/mtgo/exam/deliveryservice/service/DeliveryService.java
+++ src/main/java/com/mtgo/exam/deliveryservice/service/DeliveryService.java
@@ -2,6 +2,7 @@ package com.mtgo.exam.deliveryservice.service;
 
 import com.mtgo.exam.deliveryservice.dto.DeliveryDto;
 import com.mtgo.exam.deliveryservice.enums.DeliveryStatus;
+import com.mtgo.exam.deliveryservice.model.Delivery;
 import com.mtgo.exam.deliveryservice.repository.IDeliveryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +27,8 @@ public class DeliveryService implements IDeliveryService{
     }
     @Override
     public List<DeliveryDto> getDeliveriesByStatus(DeliveryStatus deliveryStatus) {
+        List<Delivery> claimableDeliveries = deliveryRepository.findByDeliveryStatus(deliveryStatus);
+        // TODO: Map to DTOs
         return null;
     }
 }
