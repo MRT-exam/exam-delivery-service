65f026604f94065b316d41252d7e5a4e4f25a423
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-12-22 15:32:06 +0100

    add @Repository tag to repository

--- src/main/java/com/mtgo/exam/deliveryservice/repository/IDeliveryRepository.java
+++ src/main/java/com/mtgo/exam/deliveryservice/repository/IDeliveryRepository.java
@@ -3,9 +3,11 @@ package com.mtgo.exam.deliveryservice.repository;
 import com.mtgo.exam.deliveryservice.enums.DeliveryStatus;
 import com.mtgo.exam.deliveryservice.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IDeliveryRepository extends JpaRepository<Delivery, Integer> {
     List<Delivery> findByDeliveryStatus(DeliveryStatus deliveryStatus);
 }
